ddfffbd6413992083693cc015db8d704330a3117
Lainercaceres11 <lainercaceres12@gmail.com>
2026-03-27 16:24:05 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Inter",
+      cssVariable: "--inter",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/inter-cyrillic-100-normal.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+});
