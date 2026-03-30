96990894b35f4f60bd3948b291d3eb2b589012b4
Lainercaceres11 <lainercaceres12@gmail.com>
2026-03-29 19:58:31 -0500

    added banner and about section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,5 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  fonts: [
-    {
-      provider: fontProviders.local(),
-      name: "Inter",
-      cssVariable: "--inter",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/inter-cyrillic-100-normal.woff2"],
-            weight: "normal",
-            style: "normal",
-          },
-        ],
-      },
-    },
-  ],
-});
+export default defineConfig({});
