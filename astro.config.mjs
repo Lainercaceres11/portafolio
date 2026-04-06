162bbc8a920945092f0fde809ec3beef7f3c6c3d
Lainercaceres11 <lainercaceres12@gmail.com>
2026-04-06 12:15:17 -0500

    chore: update favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,4 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  vite: {
-    server: {
-      watch: {
-        usePolling: true,
-      },
-    },
-  },
 });
